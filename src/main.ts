fa5ee969010c40180bb88cab71e656bab12fae94
Anton Volkov <antonvolkov@Antons-MacBook-Air.local>
2021-12-20 04:37:38 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, { cors: true });
 	app.setGlobalPrefix('api');
+	app.enableCors({
+		origin: 'http://localhost:3001',
+	});
 	await app.listen(3000);
 }
 bootstrap();
